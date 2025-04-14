6a6a7af46b542f3e35123e1656552a59596c2b01
milanakdj <milanakdj@gmail.com>
2025-04-14 15:38:22 +0000

    canada updates added

--- basic.py
+++ basic.py
@@ -7,17 +7,17 @@ from jobspy.model import Country
 
 jobs = scrape_jobs(
     # site_name=["indeed", "linkedin", "zip_recruiter", "glassdoor", "google", "bayt", "naukri"],
-    site_name=["linkedin", "google"],
-    search_term="python developer",
-    google_search_term="python developer jobs near Kathmandu District, Nepal since yesterday",
-    location="Nepal",
-    results_wanted=20,
+    site_name=["linkedin", "google", "indeed", "zip_recruiter", "glassdoor"],
+    search_term="summer part time",
+    google_search_term="summer part time in toronto area",
+    location="Toronto, Canada",
+    results_wanted=80,
     hours_old=72,
-    # country_indeed='Nepal',
+    country_indeed='Canada',
     
     # linkedin_fetch_description=True # gets more info such as description, direct job url (slower)
     # proxies=["208.195.175.46:65095", "208.195.175.45:65095", "localhost"],
 )
 print(f"Found {len(jobs)} jobs")
 print(jobs.head())
-jobs.to_csv("./backend_kathmandu_1.csv", quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False) # to_excel
+jobs.to_csv("./backend_toronto_1.csv", quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False) # to_excel
